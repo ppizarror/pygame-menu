0027567d0c875b6406af55249077a3d6ce2bf395
Pablo Pizarro <pablo.pizarro@ing.uchile.cl>
2017-11-19 16:24:36 +0000

    Version 1.71

--- pygameMenu/__init__.py
+++ pygameMenu/__init__.py
@@ -25,4 +25,4 @@ import pygameMenu.fonts as fonts
 # Other
 __author__ = 'Pablo Pizarro R.'
 __email__ = 'pablo.pizarro@ing.uchile.cl'
-__version__ = 'v1.7 (5/11/2017)'
+__version__ = 'v1.71 (19/11/2017)'
