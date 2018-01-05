5219a1976ce9b5ba283b43199d802cee83deb9a9
Asier Cardoso <asier314@gmail.com>
2018-01-05 21:46:43 +0100

    Create source distribution

--- setup.py
+++ setup.py
@@ -1,4 +1,5 @@
 from setuptools import setup
+from pygameMenu import __author__, __email__, __version__
 
 
 with open("README.md", 'r') as f:
@@ -6,11 +7,12 @@ with open("README.md", 'r') as f:
 
 setup(
     name='pygameMenu',
-    version='1.7',
+    version=__version__,
     description='Menu for pygame, simple, lightweight and easy to use',
+    long_description=long_description,
     license='GPLv3',
-    author='Pablo Pizarro R.',
-    author_email='pablo.pizarro@ing.uchile.cl',
+    author=__author__,
+    author_email=__email__,
     url='http://ppizarror.com/pygame-menu/',
     packages=['pygameMenu'],
     install_requires=['pygame'],
