4134e7ae2c78a8113100ec9d04a62de3458a097b
ppizarror <pablo.pizarro@ing.uchile.cl>
2019-01-30 12:42:03 +0000

    New license date

--- pygameMenu/fonts.py
+++ pygameMenu/fonts.py
@@ -4,7 +4,7 @@ FONTS
 Menu available fonts.
 
 The MIT License (MIT)
-Copyright 2017-2018 Pablo Pizarro R. @ppizarror
+Copyright 2017-2019 Pablo Pizarro R. @ppizarror
 
 Permission is hereby granted, free of charge, to any person obtaining a
 copy of this software and associated documentation files (the "Software"),
