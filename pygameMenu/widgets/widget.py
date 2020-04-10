2f0be09f1b2376ca8556436d7412a426c5cbe191
ppizarror <pablo.pizarro@ing.uchile.cl>
2020-04-10 14:05:05 -0400

    Improve widget label comment

--- pygameMenu/widgets/widget.py
+++ pygameMenu/widgets/widget.py
@@ -108,7 +108,7 @@ class Widget(object):
         self._create_shadow_tuple()
 
         # Public attributes
-        self.is_selectable = True  # type:bool
+        self.is_selectable = True  # Some widgets cannot be selected like labels
         self.joystick_enabled = True  # type: bool
         self.mouse_enabled = True  # type: bool
         self.selected = False  # type: bool
