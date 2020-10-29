faeebb8cd4c9e23c02cb250c7ebb50f4bb7fa2e4
ppizarror <pablo.pizarro@ing.uchile.cl>
2020-10-29 11:10:00 -0300

    Added py 3.9 to setup

--- setup.py
+++ setup.py
@@ -70,6 +70,7 @@ setup(
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
         'Programming Language :: Python',
         'Topic :: Games/Entertainment',
         'Topic :: Multimedia',
