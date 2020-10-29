0a00f0bf215f7878a4b26ddcc0eb75ecce715f35
ppizarror <pablo.pizarro@ing.uchile.cl>
2020-10-28 23:30:41 -0300

    Add py39 travis

--- setup.py
+++ setup.py
@@ -66,7 +66,6 @@ setup(
         'Natural Language :: English',
         'Operating System :: OS Independent',
         'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
