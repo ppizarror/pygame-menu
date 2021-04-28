a8d219a6c1ca754d14bb221cdd04d926dd153992
ppizarror <pablo@ppizarror.com>
2021-04-28 19:26:20 -0400

    SimpleSelection to init

--- pygame_menu/widgets/selection/__init__.py
+++ pygame_menu/widgets/selection/__init__.py
@@ -33,3 +33,4 @@ from pygame_menu.widgets.selection.highlight import HighlightSelection
 from pygame_menu.widgets.selection.left_arrow import LeftArrowSelection
 from pygame_menu.widgets.selection.none import NoneSelection
 from pygame_menu.widgets.selection.right_arrow import RightArrowSelection
+from pygame_menu.widgets.selection.simple import SimpleSelection
