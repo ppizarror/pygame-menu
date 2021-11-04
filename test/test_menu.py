d84d6b05a8965e9facdf6650e04ed72df49a684e
ppizarror <pablo@ppizarror.com>
2021-11-03 23:00:25 -0300

    Improve tests

--- test/test_menu.py
+++ test/test_menu.py
@@ -4,29 +4,6 @@ https://github.com/ppizarror/pygame-menu
 
 TEST MENU
 Menu object tests.
-
-License:
--------------------------------------------------------------------------------
-The MIT License (MIT)
-Copyright 2017-2021 Pablo Pizarro R. @ppizarror
-
-Permission is hereby granted, free of charge, to any person obtaining a
-copy of this software and associated documentation files (the "Software"),
-to deal in the Software without restriction, including without limitation
-the rights to use, copy, modify, merge, publish, distribute, sublicense,
-and/or sell copies of the Software, and to permit persons to whom the Software
-is furnished to do so, subject to the following conditions:
-
-The above copyright notice and this permission notice shall be included in all
-copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
-WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
-CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
--------------------------------------------------------------------------------
 """
 
 __all__ = ['MenuTest']
@@ -287,6 +264,18 @@ class MenuTest(BaseRSTest):
         self.assertEqual(b5.get_col_row_index(), (0, 0, 2))
         self.assertEqual(b6.get_col_row_index(), (0, 0, 3))
         self.assertEqual(b7.get_col_row_index(), (0, 1, 4))
+
+    def test_float_position(self) -> None:
+        """
+        Tests float position.
+        """
+        menu = MenuUtils.generic_menu(center_content=False)
+
+        a = menu.add.label('nice')
+        a.set_float()
+        print(a.get_position())
+        menu.render()
+        print(a.get_position())
 
     def test_translate(self) -> None:
         """
