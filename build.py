af68478086bf4c2b893e8e73b7cac6148d4f4c32
ppizarror <pablo@ppizarror.com>
2022-12-05 19:22:19 -0300

    Update build

--- build.py
+++ build.py
@@ -11,7 +11,6 @@ import sys
 
 assert len(sys.argv) == 2, 'Argument is required, usage: build.py pip/twine/gource'
 mode = sys.argv[1].strip()
-python = 'python3' if not sys.platform == 'win32' else 'py -3.8'
 
 if mode == 'pip':
     if os.path.isdir('dist'):
@@ -22,11 +21,11 @@ if mode == 'pip':
         for k in os.listdir('build'):
             if 'bdist.' in k or k == 'lib':
                 os.system(f'rm -rf build/{k}')
-    os.system(f'{python} setup.py sdist bdist_wheel')
+    os.system(f'python setup.py sdist bdist_wheel')
 
 elif mode == 'twine':
     if os.path.isdir('dist'):
-        os.system(f'{python} -m twine upload dist/*')
+        os.system(f'python -m twine upload dist/*')
     else:
         raise FileNotFoundError('Not distribution been found, execute build.py pip')
 
