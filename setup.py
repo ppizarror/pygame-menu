4fde1f7462d0fa10e488e9834c9e72571d31b284
Pablo Pizarro R <pablo@ppizarror.com>
2022-12-05 14:17:02 -0300

    Add 3.10 to setup

--- setup.py
+++ setup.py
@@ -81,6 +81,7 @@ setup(
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
         'Programming Language :: Python',
         'Topic :: Games/Entertainment',
         'Topic :: Multimedia',
