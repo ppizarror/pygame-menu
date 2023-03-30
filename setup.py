97392211414634c7906f483ccde1b6525bed474f
ppizarror <pablo@ppizarror.com>
2023-03-30 09:21:48 -0300

    Support python 3.11

--- setup.py
+++ setup.py
@@ -68,6 +68,7 @@ setup(
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
         'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
         'Programming Language :: Python',
         'Topic :: Games/Entertainment',
         'Topic :: Multimedia',
