bcfaccbb11d4a6ecba588eec2851932dc46c2337
ppizarror <pablo@ppizarror.com>
2023-06-28 14:00:25 -0400

    Fix sphinx

--- setup.py
+++ setup.py
@@ -79,7 +79,7 @@ setup(
     python_requires='>=3.6, <4',
     install_requires=requirements,
     extras_require={
-        'docs': ['sphinx', 'sphinx-autodoc-typehints>=1.2.0', 'sphinx-rtd-theme'],
+        'docs': ['sphinx<7', 'sphinx-autodoc-typehints>=1.2.0', 'sphinx-rtd-theme'],
         'test': ['nose2[coverage_plugin]']
     },
     setup_requires=[
